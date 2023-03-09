838e8caeeed50f2b21a9d89ff880d87c5a17deb3
alexxuss1996 <alekseymelnych132@gmail.com>
2023-03-09 12:07:04 +0200

    Close Mobile Menu when navigate to another page

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -2,14 +2,21 @@ import { NavLink } from 'react-router-dom';
 
 type NavMenuType = {
   isMobileOpen: boolean;
+  closeMobileMenu: () => void;
 };
-const NavMenu = ({ isMobileOpen }: NavMenuType) => {
+const NavMenu = ({ isMobileOpen, closeMobileMenu }: NavMenuType) => {
+  const handleNavClick = () => {
+    if (isMobileOpen) {
+      closeMobileMenu();
+    }
+  };
+
   return (
     <nav
       className={
         isMobileOpen
-          ? `visible fixed inset-0 z-40 order-1 m-0 block h-full w-full bg-white opacity-100 transition delay-150 duration-500 ease-in-out md:static md:order-none md:h-auto md:w-auto md:bg-none`
-          : `invisible fixed inset-0 order-1 mx-2 w-full bg-white opacity-0 transition delay-300 duration-500 ease-in-out md:visible md:static md:order-none md:block md:w-auto md:bg-none md:opacity-100`
+          ? `visible fixed inset-0 z-40 order-1 m-0 block h-full w-full bg-white opacity-100 transition delay-150 duration-300 ease-in md:static md:order-none md:h-auto md:w-auto md:bg-none`
+          : `invisible fixed inset-0 order-1 mx-2 w-full bg-white opacity-0 md:visible md:static md:order-none md:block md:w-auto md:bg-none md:opacity-100`
       }
     >
       <ul
@@ -20,13 +27,18 @@ const NavMenu = ({ isMobileOpen }: NavMenuType) => {
         }
       >
         <li className="mr-5 w-full sm:mr-8">
-          <NavLink to={'/'} className="inline-block cursor-pointer hover:underline">
+          <NavLink
+            to={'/'}
+            onClick={handleNavClick}
+            className="inline-block cursor-pointer hover:underline"
+          >
             Home
           </NavLink>
         </li>
         <li className="mr-5 w-full sm:mr-8">
           <NavLink
             to={'/shop'}
+            onClick={handleNavClick}
             className="mx-auto inline-block w-full cursor-pointer hover:underline"
           >
             Shop
@@ -35,6 +47,7 @@ const NavMenu = ({ isMobileOpen }: NavMenuType) => {
         <li className="mr-5 w-full sm:mr-8">
           <NavLink
             to={'/about'}
+            onClick={handleNavClick}
             className="mx-auto inline-block w-full cursor-pointer hover:underline"
           >
             About
@@ -43,6 +56,7 @@ const NavMenu = ({ isMobileOpen }: NavMenuType) => {
         <li className="mr-5 w-full sm:mr-8">
           <NavLink
             to={'/blog'}
+            onClick={handleNavClick}
             className="mx-auto inline-block w-full cursor-pointer hover:underline"
           >
             Blog
