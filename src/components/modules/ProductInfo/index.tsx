1370ed15463ec71e78bd4c738c8102135faa712f
alexxuss1996 <alekseymelnych132@gmail.com>
2023-06-15 18:25:47 +0300

    Add description and rewiev count to ProductInfo

--- src/components/modules/ProductInfo/index.tsx
+++ src/components/modules/ProductInfo/index.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 
 import { StarRating } from '@/components/features/StarRating';
-import { ProductResponseType } from '@/store/services/api/productsApi';
+import { ProductResponseType } from '@/store/services/api/miralouApi';
 
 type ProductInfoProps = Pick<
   ProductResponseType,
-  'code' | 'name' | 'rating' | 'price' | 'description' | 'discount_percent'
+  'code' | 'name' | 'rating' | 'price' | 'description' | 'discount_percent' | 'reviews' | 'tags'
 >;
 
-const ProductInfo = ({ name, rating }: ProductInfoProps) => {
+const ProductInfo = ({
+  name,
+  rating,
+  reviews,
+  description,
+  tags,
+  code,
+  discount_percent,
+  price,
+}: ProductInfoProps) => {
   return (
     <>
-      <h3 className="mb-4 text-3xl font-semibold text-black">{name}</h3>
-      <StarRating rating={parseInt(rating)} />
+      <h3 className="mb-2 mt-6 text-[36px] font-semibold text-black">{name}</h3>
+      <div className="mb-6 flex items-center gap-2">
+        <StarRating rating={parseInt(rating)} />
+        {reviews && (
+          <span className="text-[#717171]">
+            {reviews.length > 1
+              ? `(${reviews.length} customer reviews)`
+              : `(${reviews.length} customer review)`}
+          </span>
+        )}
+      </div>
+      <p className="text-sm font-light">{description}</p>
     </>
   );
 };
