d35a4ea393eaceff82f216777e5ab7bce8171246
alexxuss1996 <alekseymelnych132@gmail.com>
2023-09-24 22:25:32 +0300

    Add links to Navbar icon buttons

--- src/components/features/NavbarIconLinkGroup/index.tsx
+++ src/components/features/NavbarIconLinkGroup/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { FiSearch, FiHeart, FiShoppingCart } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '@/hooks';
+import Counter from '@/components/shared/Counter';
 
 const NavbarIconLinkGroup = () => {
+  const { totalItems } = useAppSelector((state) => state.cart);
   return (
     <ul className="flex">
       <li className="mr-12">
-        <a href="/search" className="cursor-pointer">
+        <Link to={'/search'} className="cursor-pointer">
           <FiSearch fontSize={'18px'} color="#222222" strokeWidth={2.5} />
-        </a>
+        </Link>
       </li>
       <li className="mr-12">
-        <a href="/wishlist" className="cursor-pointer">
+        <Link to={'/wishlist'} className="cursor-pointer">
           <FiHeart fontSize={'18px'} color="#222222" strokeWidth={2.5} />
-        </a>
+        </Link>
       </li>
-      <li className="mr-0">
-        <a href="/cart" className="cursor-pointer">
+      <li className="relative mr-0">
+        {totalItems > 0 && <Counter value={totalItems} />}
+        <Link to={'/cart'} className="cursor-pointer">
           <FiShoppingCart fontSize={'18px'} color="#222222" strokeWidth={2.5} />
-        </a>
+        </Link>
       </li>
     </ul>
   );
