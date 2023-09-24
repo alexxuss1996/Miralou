18a85064533fa09e162f0640b7fd3512fe4b9e81
alexxuss1996 <alekseymelnych132@gmail.com>
2023-09-24 22:52:15 +0300

    Lower z-index on Counter component

--- src/components/shared/Counter/index.tsx
+++ src/components/shared/Counter/index.tsx
@@ -4,7 +4,7 @@ type CounterType = {
 };
 const Counter = ({ value }: CounterType) => {
   return (
-    <div className="absolute -top-2 -right-2 z-40 flex h-3.5 w-3.5 items-center justify-center rounded-full bg-red text-[10px] font-bold text-white">
+    <div className="absolute -top-2 -right-2 z-20 flex h-3.5 w-3.5 items-center justify-center rounded-full bg-red text-[10px] font-bold text-white">
       <span>{value}</span>
     </div>
   );
